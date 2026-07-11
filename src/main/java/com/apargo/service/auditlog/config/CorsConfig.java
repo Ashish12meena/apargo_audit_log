9205544d56ce4ece790dbdacee90909bcd745a37
Ashish meena <ashishmeena100603@gmail.com>
2026-07-11 14:08:19 +0530

    update it

--- src/main/java/com/apargo/service/auditlog/config/CorsConfig.java
+++ src/main/java/com/apargo/service/auditlog/config/CorsConfig.java
@@ -25,7 +25,6 @@ public class CorsConfig {
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
-
         return new CorsFilter(source);
     }
 }
